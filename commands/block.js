e79fc129b1f6531d0b5a611b32f80c31f0c5c932
Mia <alexranzee@gmail.com>
2024-04-19 19:18:35 -0700

    d

---
+++ commands/block.js
@@ -0,0 +1,5 @@
+<client>.command({
+    module.exports = ({
+       name: "block",
+       code: `**Sending a block request for $message" $channelSendMessage[1231062812244054026;Hello!;false]` // The ping function returns the current ping.
+   });
